7be418eeb028da6edb3bcabc0aae5436ae32c0a7
Bruno <bruno.cardoso02@outlook.com>
2023-10-05 20:19:22 -0300

    Adcionando seletor

--- xingue-aqui/src/pages/PaginaPrincipal/index.jsx
+++ xingue-aqui/src/pages/PaginaPrincipal/index.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 function PaginaPrincipal() {
     const [swearWord, setSwearWord] = useState('');
+    const [categorie, setCategorie] = useState('VIOLENCIA')
 
     const api = axios.create({
         baseURL: process.env.REACT_APP_API_URL
@@ -11,11 +12,11 @@ function PaginaPrincipal() {
 
     const swearWordItens = {
         swear_word: swearWord,
-        categorie: "VIOLENCIA"
+        categorie: categorie
     }
 
     const enviarXingamento = () => {
-        api.post('/swearword/create', swearWordItens)
+        api.post('swearword/create', swearWordItens)
             .then((res) => alert("Xingamento enviado", res.data))
             .catch((err) => alert(err))        
         setSwearWord('')
@@ -27,6 +28,18 @@ function PaginaPrincipal() {
                 <h1>Xingue <br /> aqui🧨</h1>
                 <div className='containerFormulario'>
                     <input type='text' className='inputPrincipal' value={swearWord} onChange={(e) => setSwearWord(e.target.value)} />
+                    <select value={categorie} onChange={(e) => setCategorie(e.target.value)} className='menu-opcoes'>
+                        <option value="VIOLENCIA">Violência</option>
+                        <option value="ETARISMO">Etarismo</option>
+                        <option value="ANTISSEMITISMO">Antissemitismo</option>
+                        <option value="RACISMO">Racismo</option>
+                        <option value="MACHISMO">Machismo</option>
+                        <option value="FEMINISMO">Feminismo</option>
+                        <option value="GORDOFOBIA">Gordofobia</option>
+                        <option value="XENOFOBIA">Xenofobia</option>
+                        <option value="HOMOFOBIA">Homofobia</option>
+                        
+                    </select>
                     <button type='submit' className='inputBotao' onClick={enviarXingamento}>Enviar💥</button>
                 </div>
             </section>
